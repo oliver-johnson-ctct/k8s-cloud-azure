432f07eecdcfe7507f3874cf7f98fa04ca2a68a8
HeyMo0sh <hamishwatson8@outlook.com>
2025-08-27 06:47:28 +1200

    Adding in network vars 14

--- main.tf
+++ main.tf
@@ -1,16 +1,18 @@
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~> 3.100"   # any recent 3.x is fine
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  skip_provider_registration = true
   features {}
+  # leave this false (or omit) until the RPs are registered
+  # skip_provider_registration = true
 
  # helm = {
     #   source = "hashicorp/helm"
