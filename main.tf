675d7bd730b8e05db7119afaa42e37225eea268e
HeyMo0sh <hamishwatson8@outlook.com>
2025-08-27 06:05:46 +1200

    Adding in network vars 7

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
 terraform {
-  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.75.0"
+      version = "=3.0.0"
     }
-    # helm = {
-    #   source = "hashicorp/helm"
-    #   version = "~> 2.11.0"
-    # }
   }
-  required_version = "~> 1.3"
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  resource_provider_registrations = "none"
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
