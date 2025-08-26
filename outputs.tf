9e76a2b3f403b0e7288a31c33b879555aeb3a483
HeyMo0sh <hamishwatson8@outlook.com>
2025-08-27 07:18:43 +1200

    Adding in network vars 17

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,16 @@
 output "resource_group" {
-	value = azurerm_resource_group.rg.name
+  value = local.rg_name
 }
+
+output "resource_group_location" {
+  value = local.rg_loc
+}
+
 output "aks_name" {
-	value = azurerm_kubernetes_cluster.aks.name
+  value = azurerm_kubernetes_cluster.aks.name
 }
+
 output "kube_config" {
-	value     = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
-	sensitive = true
+  value     = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+  sensitive = true
 }
