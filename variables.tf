5b138e1c4b2a0c1dfaabcf33e4c5ac970c434418
HeyMo0sh <hamishwatson8@outlook.com>
2025-08-27 09:06:04 +1200

    Adding in deployment yml

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "suffix" {
 variable "resource_group_name" {
 	description = "Resource group name"
 	type        = string
-	default     = "rg-trimble-002"
+	default     = "rg-trimble-001"
 }
 
 variable "create_rg" {
@@ -25,31 +25,31 @@ variable "prefix" {
 variable "aks_name" {
 	description = "AKS name"
 	type        = string
-	default     = "aks-cluster-002"
+	default     = "aks-cluster-001"
 }
 
 variable "vnet_address_space" {
 	description = "VNet CIDR"
 	type        = string
-	default     = "10.74.2.0/24"
+	default     = "10.74.1.0/24"
 }
 
 variable "subnet_address_prefix" {
   description = "Subnet CIDR"
   type        = string
-  default     = "10.74.2.0/26" 
+  default     = "10.74.1.0/26"
 }
 
 variable "dns_service_ip" {
 	description = "DNS service IP for AKS"
 	type        = string
-	default     = "10.74.2.33"
+	default     = "10.74.1.33"
 }
 
 variable "service_cidr" {
 	description = "Service CIDR for AKS"
 	type        = string
-	default     = "10.74.2.32/26"
+	default     = "10.74.1.32/26"
 }
 
 variable "nodepool_name" {
