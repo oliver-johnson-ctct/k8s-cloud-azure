a67a620668d5395c0b3e653e2fb0b4b619432f98
HeyMo0sh <hamishwatson8@outlook.com>
2025-08-27 05:55:38 +1200

    Adding in network vars 4

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
-  required_version = ">= 1.5.0"
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.110"
+      version = "3.75.0"
     }
+    # helm = {
+    #   source = "hashicorp/helm"
+    #   version = "~> 2.11.0"
+    # }
   }
+  required_version = "~> 1.3"
 }
-provider "azurerm" {
-  features {}
-}
-# For workshops we keep local state; for prod, configure an azurerm backend.
